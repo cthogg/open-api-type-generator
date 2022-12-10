e00ca919f4d79479c190ad7359d171a448c96340
Christopher Hogg <cthogg@users.noreply.github.com>
2022-12-10 13:36:50 +0000

    feat: generate endpoints

--- src/parser/parseYamlFile.ts
+++ src/parser/parseYamlFile.ts
@@ -1,13 +1,15 @@
 import SwaggerParser from "@apidevtools/swagger-parser";
 import fs from "fs";
 import { parse } from "yaml";
-import { OpenApiSpec } from "./validateParsedStructure";
+import { generateEndpoints } from "./validateParsedStructure";
 
 const getAndValidateFile = async (path: string) => {
   SwaggerParser.parse(path);
   const file = fs.readFileSync(path, "utf8");
   const fileAsJson = parse(file);
-  return OpenApiSpec.parse(fileAsJson);
+  const endpoints = generateEndpoints(fileAsJson);
+  console.log("endpoints", endpoints);
+  return fileAsJson;
 };
 
 export { getAndValidateFile };
--- src/parser/validateParsedStructure.ts
+++ src/parser/validateParsedStructure.ts
@@ -12,13 +12,149 @@ const isValidOpenApiSpec = async (fileAsJson: string) => {
     return false;
   }
 };
+const exampleFileAsJson = {
+  openapi: "3.0.0",
+  info: {
+    version: "1.0.0",
+    title: "Simple API",
+    description: "A simple API to illustrate OpenAPI concepts",
+  },
+  servers: [{ url: "https://example.io/v1" }],
+  components: {
+    securitySchemes: { BasicAuth: { type: "http", scheme: "basic" } },
+  },
+  security: [{ BasicAuth: [] }],
+  paths: {
+    "/artists": {
+      get: {
+        description: "Returns a list of artists",
+        responses: {
+          "200": {
+            description: "Successfully returned a list of artists",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "array",
+                  items: {
+                    type: "object",
+                    required: ["username"],
+                    properties: {
+                      artist_name: { type: "string" },
+                      artist_genre: { type: "string" },
+                      albums_recorded: { type: "integer" },
+                      username: { type: "string" },
+                    },
+                  },
+                },
+              },
+            },
+          },
+          "400": {
+            description: "Invalid request",
+            content: {
+              "application/json": {
+                schema: {
+                  type: "object",
+                  properties: { message: { type: "string" } },
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  },
+};
 
-const OpenApiSpec = z.object({
-  openapi: z.string().startsWith("3."), //only supporting open api version 3 at the moment // could provide custom errors to show this with https://github.com/colinhacks/zod/blob/master/ERROR_HANDLING.md#customizing-errors-with-zoderrormap
-  paths: z.object({
-    "/artists": z.object({}),
+const Info = z.object({
+  version: z.string(),
+  title: z.string(),
+  description: z.string(),
+});
+
+const schemaArray = z.object({
+  type: z.literal("array"),
+  items: z.object({
+    type: z.literal("object"),
+    required: z.array(z.string()),
+    properties: z.record(z.string(), z.object({ type: z.string() })),
   }),
 });
+
+const nonSpecificSpec = z.object({
+  openapi: z.string().startsWith("3."),
+  info: Info,
+  paths: z.record(
+    z.string().startsWith("/"),
+    z.object({
+      get: z.object({
+        description: z.string(),
+        responses: z.object({
+          "200": z.object({
+            description: z.string(),
+            content: z.object({
+              "application/json": z.object({
+                schema: schemaArray,
+              }),
+            }),
+          }),
+        }),
+      }),
+    })
+  ),
+});
+
+type NonSpecitifcSpec = z.infer<typeof nonSpecificSpec>;
+type SchemaArray = z.infer<typeof schemaArray>;
+// {
+//   endpoint: "GET /artists",
+//   responseBody: r.Array(
+//     r.Record({
+//       username: r.String,
+//       artist_name: r.String.optional(),
+//       artist_genre: r.String.optional(),
+//       albums_recorded: r.Number.optional(),
+//     })
+//   ),
+// }
+
+type Endpoint = {
+  endpoint: string;
+  responseBody: z.ZodType;
+};
+
+const generateZodFromSchema = (schema: SchemaArray): z.ZodType => {
+  const listOfProperties = schema.items.properties;
+  const listOfPropertiesKeys = Object.keys(listOfProperties).map((key) => {
+    const property = listOfProperties[key];
+    return {
+      [key]: property,
+    };
+  });
+  console.log("listOfPropertiesKeys", listOfPropertiesKeys);
+  return z.object({});
+};
+
+const generateZodPaths = (spec: NonSpecitifcSpec): Endpoint[] => {
+  const allPaths = spec.paths;
+  const allPathsKeys: Endpoint[] = Object.keys(allPaths).map((path) => {
+    const pathObject = allPaths[path];
+    const pathSchema: z.ZodType = nonSpecificSpec.shape.paths.keySchema;
+    const schema =
+      pathObject.get.responses["200"].content["application/json"].schema;
+    const endpoint: Endpoint = {
+      endpoint: `GET ${path}`,
+      responseBody: generateZodFromSchema(schema),
+    };
+    return endpoint;
+  });
+  return allPathsKeys;
+};
+
+export const generateEndpoints = (fileAsJson: string): Endpoint[] => {
+  const files = nonSpecificSpec.parse(exampleFileAsJson);
+  return generateZodPaths(files);
+};
 
 // fileAsJson {
 //     openapi: '3.0.0',
@@ -33,6 +169,4 @@ const OpenApiSpec = z.object({
 //     paths: { '/artists': { get: [Object] } }
 //   }
 
-type OpenApiSpecType = z.infer<typeof OpenApiSpec>;
-
-export { OpenApiSpec, OpenApiSpecType, isValidOpenApiSpec };
+export { isValidOpenApiSpec };
