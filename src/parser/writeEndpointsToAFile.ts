7030f6d22e10326a99d4df235a2b9e0d31834589
Christopher Hogg <cthogg@users.noreply.github.com>
2023-02-08 21:11:15 +0000

    feat: get endpoints

--- src/parser/writeEndpointsToAFile.ts
+++ src/parser/writeEndpointsToAFile.ts
@@ -3,40 +3,34 @@ import fs from "fs";
 import { createTypeAlias, printNode, zodToTs } from "zod-to-ts";
 import { Endpoint } from "./types";
 
-// regex to get all letters after the last slash
-const getAfterSlash = (str: string): string => {
-  const regex = /\/(\w+)$/;
-  const match = str.match(regex);
-  if (!match) {
-    throw new Error("No match found");
-  }
-  return match[1];
-};
-
 function convertToDTOName(str: string): string {
   const [httpMethod, endpoint] = str.split(" ");
-  console.log("endpoint", endpoint);
-  console.log("httpMethod", httpMethod);
   const endpointParts = endpoint.split("/");
-  console.log("endpointParts", endpointParts);
   const dtoName = endpointParts[1];
   return `${sentenceCase(httpMethod)}${sentenceCase(dtoName)}DTO`;
 }
 
+const generateStrings = (endpoints: Endpoint[]) => {
+  const stringArray: string[] = [];
+
+  endpoints.forEach((endpoint, i) => {
+    const identifier = endpoint.endpoint;
+    const name = convertToDTOName(identifier);
+    const { node } = zodToTs(endpoint.responseBody, name);
+    const typeAlias = createTypeAlias(node, name, endpoint.endpoint);
+    stringArray.push(printNode(typeAlias));
+  });
+  return stringArray;
+};
+
 export const writeEndpointsToAFile = (endpoints: Endpoint[]) => {
+  const stringArray = generateStrings(endpoints);
   const file = fs.createWriteStream("endpoints.ts");
   file.on("error", (err) => {
     console.log(err);
   });
-
-  endpoints.forEach((endpoint, i) => {
-    const identifier = endpoint.endpoint;
-    console.log("identifier", identifier);
-    const name = convertToDTOName(identifier);
-    console.log("name", name);
-    const { node } = zodToTs(endpoint.responseBody, name);
-    const typeAlias = createTypeAlias(node, name, endpoint.endpoint);
-    file.write(printNode(typeAlias));
+  stringArray.forEach((str) => {
+    file.write(str);
   });
   file.end();
 };
